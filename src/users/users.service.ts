b95737c9744d50dc116cc2194711a7f7b2127a67
zunbee <zunbeeok@gmail.com>
2023-05-18 18:54:38 +0900

    분리

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,37 +6,37 @@ import { User } from 'src/common/entity/user.entity';
 @Injectable()
 export class UsersService implements UsersService {
     constructor(
-        private readonly dataSource:DataSource,
-    ){
+        private readonly dataSource: DataSource,
+    ) {
     }
 
-    async join(user:UserDto){
+    async join(user: UserDto) {
         //맵핑
-        const userEntity=new User(user.id,user.pw,user.name,user.hpNumber);
+        const userEntity = new User(user.id, user.pw, user.name, user.hpNumber);
         const queryRunner = this.dataSource.createQueryRunner();
 
         //회원 저장 에러처리.
-        try{
+        try {
             //db커넥션 에러처리
-            try{
+            try {
                 await queryRunner.connect();
                 await queryRunner.startTransaction();
                 await queryRunner.manager.save(userEntity);
                 await queryRunner.commitTransaction();
-            }catch(err){
+            } catch (err) {
                 console.log(err);
                 await queryRunner.rollbackTransaction();
-            }finally{
+            } finally {
                 await queryRunner.release();
             }
-        }catch(err){
+        } catch (err) {
             console.log(err);
         }
         return;
     }
 
-    async getUser(){
+    async getUser() {
 
     }
-    
+
 }
